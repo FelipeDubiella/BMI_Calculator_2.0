f89394fe54891dad6262d5fd744acf7fb930feb2
Felipe Dubiella de Oliveira <Dubiella2@icloud.com>
2024-06-30 11:49:25 -0300

    feat:
    receive all information and pass it to resultActivity

--- app/src/main/java/com/felipedubiella/bmi_calculator_20/MainActivity.kt
+++ app/src/main/java/com/felipedubiella/bmi_calculator_20/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.felipedubiella.bmi_calculator_20
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,7 +16,33 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
 
+        with(binding) {
 
+            btnCalculate.setOnClickListener {
+
+                val edtHeight: String = edtHeight.text.toString()
+                val edtWeight: String = edtWeight.text.toString()
+
+                val intent = Intent(this@MainActivity, resultActivity::class.java)
+
+                if (edtWeight.isNotEmpty() && edtHeight.isNotEmpty()) {
+                    intent.putExtra("height", edtHeight.toFloat())
+                    intent.putExtra("weight", edtWeight.toFloat())
+                    startActivity(intent)
+
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Fill all the fields to calcute your BMI",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+
+            }
+
+
+        }
 
 
 
